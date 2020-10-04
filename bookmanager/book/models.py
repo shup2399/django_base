54f899b532bc5aaa8693ed2a158c8bc270e98c8c
zbing <619219416@qq.com>
2020-10-04 09:46:52 +0800

    admin站点

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -7,10 +7,12 @@ class BookInfo(models.Model):
     name=models.CharField(max_length=10)
 
 
+    #显示书名字
+    def __str__(self):
+        return self.name
+
+
 class PeopleInfo(models.Model):
     name=models.CharField(max_length=10)
-    gender=models.BooleanField(0)
+    gender=models.BooleanField()
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
-
-
-
