7866302ef06a54e6c497609da5548212a031cf00
zbing <619219416@qq.com>
2020-10-04 11:02:57 +0800

    子应用的urls

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+#定义视图函数
+# 视图函数的第一个参数:
+# 1.接收请求
+# 2.返回一个响应
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+def index(request):
+
+
+
+    return HttpResponse('ok')
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book.views import index
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('index/', index),
+    path('', include('book.urls')),
 ]
