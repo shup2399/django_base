7301a58cdb7e7f46841f73c2da8962ada9491514
zbing <619219416@qq.com>
2020-10-04 11:46:36 +0800

    静态文件(图片)访问

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+@ec@dp7-ynk23+8hbnjpyq+=2og((!@#ls7n2xewnqww+vh0g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '//'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
